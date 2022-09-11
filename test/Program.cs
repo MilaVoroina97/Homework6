cd52dc95ef9b4832fe629895f3a24af844bacfcb
mila <milavoronina1997@mail.ru>
2022-09-11 22:22:02 +0500

    test_5

--- test/Program.cs
+++ test/Program.cs
@@ -18,7 +18,7 @@ void TriangleSides()
       int c = triangles[2];
   }
   CheckSides();
-  Area(triangles);
+  double[] result = Area(triangles);
   perimetr = triangles[0] + triangles[1] + triangles[2];
   Console.WriteLine($"Периметр треугольника равен: {perimetr}");
 }
@@ -50,65 +50,111 @@ void CheckSides()
     }
 }
 TriangleSides();
-double Area(int[] array)
+double[] Area(int[] triangles)
 {   double polyperimetr = 0;
-    double area = 0;
-    for (int i = 0; i < array.Length - 2; i++)
+    double[] area = new double[4];
+    for (int i = 0; i < triangles.Length - 2; i++)
     {
         if ((Math.Pow(triangles[0],2) + Math.Pow(triangles[1],2) == Math.Pow(triangles[2],2)) && (triangles[0] < triangles[2]) && (triangles[2]> triangles[1]))
         {
             Console.WriteLine("Данный треугольник является прямоугольным.");
-            area = 0.5 * (triangles[0]+triangles[1]);
-            Console.WriteLine($"Площадь прямоугольного треугольника равна: {area}");
+            area[0]= 0.5 * (triangles[0]+triangles[1]);
+            Console.WriteLine($"Площадь прямоугольного треугольника равна: {area[0]}");
+            area[1] = triangles[0] / triangles[1];
+            Console.WriteLine("tan(a): " + Math.Round(area[1],2));
+            area[2] = triangles[0] / triangles[2];
+            Console.WriteLine($"sin(a):{0:#.##} {area[2]}");
+            area[3] = triangles[1] / triangles[2];
+            Console.WriteLine($"cos(a): {0:#.##}{area[3]}");
             break;
         }
         if ((Math.Pow(triangles[0],2) + Math.Pow(triangles[2],2) == Math.Pow(triangles[1],2)) && (triangles[0] < triangles[1]) && (triangles[1]> triangles[2]))
         {
             Console.WriteLine("Данный треугольник является прямоугольным.");
-            area = 0.5 * (triangles[0]+triangles[2]);
-            Console.WriteLine($"Площадь прямоугольного треугольника равна: {area}");
+            area[0] = 0.5 * (triangles[0]+triangles[2]);
+            Console.WriteLine($"Площадь прямоугольного треугольника равна: {area[0]}");
+            area[1] = triangles[0] / triangles[2];
+            Console.WriteLine($"tan(a): {0:#.##}{area[1]}");
+            area[2] = triangles[0] / triangles[1];
+            Console.WriteLine($"sin(a):{0:#.##} {area[2]}");
+            area[3] = triangles[2] / triangles[1];
+            Console.WriteLine($"cos(a): {0:#.##}{area[3]}");
+
             break;
         }
         if((Math.Pow(triangles[1],2) + Math.Pow(triangles[2],2) == Math.Pow(triangles[0],2)) && (triangles[1] < triangles[0]) &&  (triangles[0]> triangles[2]))
         {
             Console.WriteLine("Данный треугольник является прямоугольным.");
-            area = 0.5 * (triangles[1]+triangles[2]);
-            Console.WriteLine($"Площадь прямоугольного треугольника равна: {area}");
+            area[0] = 0.5 * (triangles[1]+triangles[2]);
+            Console.WriteLine($"Площадь прямоугольного треугольника равна: {area[0]}");
+            area[1] = triangles[1] /triangles[2];
+            Console.WriteLine($"tan(a): {0:#.##} {area[1]}");
+            area[1] = triangles[1] /triangles[0];
+            Console.WriteLine($"sin(a): {0:#.##}{area[2]}");
+            area[3] = triangles[2] /triangles[0];
+            Console.WriteLine($"cos(a): {0:#.##}{area[3]}");
             break;
         }
         if (triangles[0] == triangles[1] && triangles[0] != triangles[2])
         {
             Console.WriteLine("Данный треугольник является равнобедернным");
-            area = (triangles[2] / 4) * Math.Sqrt(4* Math.Pow(triangles[0],2) - Math.Pow(triangles[2],2));
-            Console.WriteLine($"Площадь равнобедренного треугольника равна: {area}");
+            area[0] = (triangles[2] / 4) * Math.Sqrt(4* Math.Pow(triangles[0],2) - Math.Pow(triangles[2],2));
+            Console.WriteLine($"Площадь равнобедренного треугольника равна: {area[0]}");
+            area[1] = triangles[2] / (2* triangles[0]);
+            Console.WriteLine($"cos(a): {area[1]}");
+            area[2] = 180 - area[1] *2;
+            Console.WriteLine($"угол(b): {area[2]}");
+            area[3] = triangles[2] / (2* triangles[0]);
+
             break;
         }
         if (triangles[0] == triangles[2] && triangles[0] != triangles[1])
         {
             Console.WriteLine("Данный треугольник является равнобедернным");
-            area = (triangles[1] / 4) * Math.Sqrt(4* Math.Pow(triangles[0],2) - Math.Pow(triangles[1],2)) ;
-            Console.WriteLine($"Площадь равнобедренного треугольника равна: {area}");
+            area[0] = (triangles[1] / 4) * Math.Sqrt(4* Math.Pow(triangles[0],2) - Math.Pow(triangles[1],2)) ;
+            Console.WriteLine($"Площадь равнобедренного треугольника равна: {area[0]}");
+            area[1] = triangles[1] / (triangles[0] *2);
+            Console.WriteLine($"cos(a): {area[1]}");
+            area[2] = 180 - area[1] *2;
+            Console.WriteLine($"угол(b): {area[2]}");
+            area[3] = triangles[1] / (triangles[0] *2);
             break;
         }
         if (triangles[1] == triangles[2] && triangles[1] != triangles[0])
         {
             Console.WriteLine("Данный треугольник является равнобедернным");
-            area = (triangles[0] / 4) * Math.Sqrt(4* Math.Pow(triangles[1],2) - Math.Pow(triangles[0],2)); 
-            Console.WriteLine($"Площадь равнобедренного треугольника равна: {area}");
+            area[0] = (triangles[0] / 4) * Math.Sqrt(4* Math.Pow(triangles[1],2) - Math.Pow(triangles[0],2)); 
+            Console.WriteLine($"Площадь равнобедренного треугольника равна: {area[0]}");
+            area[1] = triangles[0] / (triangles[1] *2);
+            Console.WriteLine($"cos(a): {area[1]}");
+            area[2] = 180 - area[1] *2;
+            Console.WriteLine($"угол(b): {area[2]}");
+            area[3] = triangles[0] / (triangles[1] *2);
+
             break;
         }
         if(triangles[0] == triangles[1] && triangles[0] == triangles[2] && triangles[1] == triangles[2])
         {
             Console.WriteLine("Данный треугольник является равностронним.");
-            area = 1.73/4 * Math.Pow(triangles[0],2);
-            Console.WriteLine($"Площадь равностороннего треугольника равна: {area}");
+            area[0] = 1.73/4 * Math.Pow(triangles[0],2);
+            Console.WriteLine($"Площадь равностороннего треугольника равна: {area[0]}");
+            area[1] = 60;
+            area[2] = 60;
+            area[3] = 60;
+            Console.WriteLine("Все углы в равностороннем треугольнике равны 60 градусов.");
             break;
         }
         else
         {
             polyperimetr = (triangles[0]+triangles[1]+triangles[2]) / 2 ;
-            area = Math.Sqrt(polyperimetr * (polyperimetr - triangles[0])*(polyperimetr - triangles[1])* (polyperimetr - triangles[2]));
-            Console.WriteLine($"Площадь треугольника равна: {area}");
+            area[0] = Math.Sqrt(polyperimetr * (polyperimetr - triangles[0])*(polyperimetr - triangles[1])* (polyperimetr - triangles[2]));
+            Console.WriteLine($"Площадь треугольника равна: {area[0]}");
+            area[1] = (Math.Pow(triangles[0],2) + Math.Pow(triangles[2],2) - Math.Pow(triangles[1],2))/(2*triangles[0]* triangles[2]);
+            Console.WriteLine($"cos(a): {area[1]}");
+            area[2] = (Math.Pow(triangles[0],2) + Math.Pow(triangles[1],2) - Math.Pow(triangles[2],2))/(2*triangles[0]* triangles[1]);
+            Console.WriteLine($"cos(b): {area[2]}");
+            area[3] = (Math.Pow(triangles[1],2) + Math.Pow(triangles[2],2) - Math.Pow(triangles[1],2))/(2*triangles[2]* triangles[1]);
+            Console.WriteLine($"cos(y): {area[3]}");
         }
     }
     return area;
