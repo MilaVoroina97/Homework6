2ef2808bdbf8da3e473648269642a87f1205b76c
mila <milavoronina1997@mail.ru>
2022-09-12 00:54:46 +0500

    Task3_3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,48 +1,32 @@
-﻿//задача 40 - HARD необязательная. На вход программы подаются три целых положительных числа. Определить , является ли это сторонами
-// треугольника. Если да, то вывести всю информацию по нему - площадь, периметр, значения углов треугольника в градусах, 
-//является ли он прямоугольным, равнобедренным, равносторонним.
+﻿//пользователь не задает сколько чисел он введет (то есть число M), 
+//а подсчет количества чисел производится после того, как пользователь не ввел информацию и нажал Enter.
 
-double [,] triangle = new double[3,3];
-void TriangleSides()
+int PositiveNumbers(int[] massiv)
 {
-  double polyperimetr = 0;
-  for (int i = 0; i < triangle.GetLength(0)-2; i++)
-  {
-      Console.Write($"Введите число а: ");
-      triangle[0,0] = Convert.ToInt32(Console.ReadLine());
-      Console.Write($"Введите число b: ");
-      triangle[0,1] = Convert.ToInt32(Console.ReadLine());
-      Console.Write($"Введите число c: ");
-      triangle[0,2] = Convert.ToInt32(Console.ReadLine());
-  }
-  CheckSides();
-  polyperimetr = (triangle[0,0]+triangle[0,1]+triangle[0,2]) / 2 ;
-  Console.WriteLine(polyperimetr);
-  triangle[1,0] = Math.Sqrt(polyperimetr * (polyperimetr - triangle[0,0])*(polyperimetr - triangle[0,1])* (polyperimetr - triangle[0,2]));
-  Console.WriteLine($"Площадь треугольника равна : {triangle[1,0]}");
-  triangle[1,1] = triangle[0,0]+triangle[0,1]+triangle[0,2];
-  Console.WriteLine($"Периметр треугольника равен : {triangle[1,1]}");
-}
-void CheckSides()
-{
-  for (int i = 0; i < triangle.GetLength(0); i++)
-  {
-      if ((triangle[0,0]+triangle[0,1]<=triangle[0,2]||triangle[0,0]+triangle[0,2]<=triangle[0,1]||triangle[0,1]+triangle[0,2]<=triangle[0,0]))
-      {
-        Console.WriteLine("Данные числа являются сторонами треугольника.");
-        
-      }
-      else Console.WriteLine("Эти числа не являются сторонами треугольника");
-      break;
-    }
-}
-TriangleSides();
-double PerimetrandArea(double[,] array)
-{
-    double area = 0;
-    double perimetr = 0;
-    for (int i = 0; i < array.GetLength(0) - 2; i++)
+    int count = 0;
+    for(int i = 0; i < massiv.Length; i++) 
     {
-        if 
+        if(massiv[i] > 0) count = count + 1;
     }
+    return count;
 }
+
+try
+{
+Console.WriteLine("Введите целые числа через точку с запятой или пробел: ");
+string x = Console.ReadLine();
+int[] numbers = x.Split(';',' ').Select(int.Parse).ToArray();
+foreach (var item in numbers)
+{
+    Console.WriteLine(item);
+}
+Console.ReadKey();
+Console.WriteLine();
+int result = PositiveNumbers(numbers);
+Console.WriteLine($"Количество положительных чисел: {result}");
+}
+catch
+{
+    Console.WriteLine("Введите, пожалуйста, целые числа.");
+}
+    
