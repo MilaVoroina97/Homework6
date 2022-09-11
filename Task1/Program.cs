bcd67389d91fbb287517d4118a62a76edb17c75a
mila <milavoronina1997@mail.ru>
2022-09-12 00:15:44 +0500

    Task1_3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,17 +5,6 @@
 
 //1, -7, 567, 89, 223-> 3
 
-//вариация задачи 41 - пользователь не задает сколько чисел он введет (то есть число M), 
-//а подсчет количества чисел производится после того, как пользователь не ввел информацию и нажал Enter.
-
-Console.WriteLine("Введите число M для определения количества чисел, которые Вы хотите ввести: ");
-int M = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[M];
-for(int i = 0; i <  M; i++)
-{
-    Console.Write("Введите координаты первой точки ");
-    numbers[i] = Convert.ToInt32(Console.ReadLine());
-}
 int PositiveNumbers(int[] massiv)
 {
     int count = 0;
@@ -25,7 +14,22 @@ int PositiveNumbers(int[] massiv)
     }
     return count;
 }
+try
+{
+    Console.WriteLine("Введите число M для определения количества чисел, которые Вы хотите ввести: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[M];
+for(int i = 0; i <  M; i++)
+{
+    Console.Write("Введите число:");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
 Console.WriteLine();
 int result = PositiveNumbers(numbers);
-Console.WriteLine(result);
+Console.WriteLine($"Количество положительных чисел: {result}");
+}
+catch
+{
+    Console.WriteLine("Введите, пожалуйста, целые числа.");
+}
 
